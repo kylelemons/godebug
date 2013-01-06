a7fab8f5423917b79f9dd3ece29aefef96fa6938
Kyle Lemons <kyle@kylelemons.net>
2013-01-06 02:12:33 -0800

    Compare doc

--- pretty/util.go
+++ pretty/util.go
@@ -18,6 +18,10 @@ func (o *Options) Print(vals ...interface{}) {
 	}
 }
 
+// Compare returns a string containing a line-by-line unified diff of the
+// values in got and want.  Each line is prefixed with '+', '-', or ' ' to
+// indicate if it should be added to, removed from, or is correct for the "got"
+// value with respect to the "want" value.
 func Compare(got, want interface{}) string {
 	diffOpt := &Options{Diffable: true}
 	gotLines := strings.Split(Reflect(got, diffOpt), "\n")
